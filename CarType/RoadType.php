cdb1b62c2d5a09dbd01a6bc04006b96f1199ea19
ShreyasSubhedar <shreyassubhedar@gmail.com>
2020-11-01 09:12:13 +0530

    RM-104 | Adding Road_Type Abstraction

---
+++ CarType/RoadType.php
@@ -0,0 +1,16 @@
+<?php
+
+# RoadType interface creates the contract which every Road(Rural/Urban) can follow.
+interface RoadType 
+{
+# GetSpeedLimit() return speed limit of the road.
+  public function GetSpeedLimit();
+
+# GetRoadLength() returns the road limit.
+  public function GetRoadLength();
+
+# SetRoadLength() uses $roadLength to set the length of the road.
+  public function SetRoadLength($roadLength);
+
+}
+?>
---
+++ CarType/UrbanRoad.php
@@ -0,0 +1,30 @@
+
+<?php
+# Including interface RoadType.php
+include ('RoadType.php');
+define("garage_distace", 50);
+
+class UrbanRoad implements RoadType{
+    protected $max_car_dropage;
+    protected $speed_limit;
+    protected $garage_distace=20;
+
+# GetRoadLength() returns the road limit.
+public function GetRoadLength()
+{
+   /* code */
+}
+
+# GetSpeedLimit() return speed limit of the road.
+public function GetSpeedLimit()
+{
+    return $this->speed_limit;
+}
+# SetRoadLength() uses $roadLength to set the length of the road.
+public function SetRoadLength($roadLength)
+{
+    /* code */
+}
+
+}
+?>
