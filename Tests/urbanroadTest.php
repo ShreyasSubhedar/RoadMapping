c8e0cab3a2bcbc46cd8eda91af9176005f104bd1
ShreyasSubhedar <shreyassubhedar@gmail.com>
2020-11-04 11:34:42 +0530

    RM-108 | Trying

--- Tests/urbanroadTest.php
+++ Tests/urbanroadTest.php
@@ -9,5 +9,20 @@ final class urbanroadTest extends TestCase
         $obj = new UrbanRoad();
         $this->assertEquals(52.5,$obj->GetSpeedLimit(70));
     }
+    public function testGetSpeedLimit1(): void
+    {
+        $obj = new UrbanRoad();
+        $this->assertEquals(37.5,$obj->GetSpeedLimit(50));
+    }
+    public function testGetSpeedLimit2(): void
+    {
+        $obj = new UrbanRoad();
+        $this->assertEquals(0,$obj->GetSpeedLimit(0));
+    }
+    public function testGetSpeedLimit3(): void
+    {
+        $obj = new UrbanRoad();
+        $this->assertEquals(87.5,$obj->GetSpeedLimit(110));
+    }
 }
 ?>
