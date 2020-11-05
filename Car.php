b0c2b02425b9efe954243897375735f07d3e8ebc
hrishikesh <hrishikeshsawant17@gmail.com>
2020-11-05 14:18:09 +0530

    RM-113 | Search and Scrollable table added

--- Car.php
+++ Car.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 require_once('Classes.php');
 require_once('db_function.php');
 class Car{
@@ -10,7 +12,7 @@ class Car{
     private $mappingStatus;
     private $fuelStatus = array('Full' => 0, 'Medium' => 0, 'Low' => 0, 'Empty' => 0);
 }
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']) && $_POST['roadType']){
 
     $road_type = $_POST['roadType'];
     $road_length = $_POST['roadLength'];
@@ -38,7 +40,8 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Road Mapping Task</title>
-    <!-- <link rel="stylesheet" href="main.css" /> -->
+    
+    <link rel="stylesheet" href="main.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
@@ -74,83 +77,96 @@ if(isset($_POST['submit'])){
                 </form>
             </div>
         </div>
-          <div class="row">
+        <div class="row">
             <div class="col-sm">
                 <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">TOTAL DISTANCE TRAVELLED</h5>
-                    <p id="dt"class="card-text"><?php echo $js->getDist() ?></p>
-                    
-                </div>
+                    <div class="card-body">
+                        <h5 class="card-title">TOTAL DISTANCE TRAVELLED</h5>
+                        <p id="dt"class="card-text"><?php echo $js->getDist() ?></p>
+                    </div>
                 </div>
             </div>
-                <div class="col-sm">
-                    <div class="card text-center" style="width: 18rem;">
+            <div class="col-sm">
+                <div class="card text-center" style="width: 18rem;">
                         <div class="card-body">
                             <h5 class="card-title">TOTAL TIME TAKEN</h5>
                             <p id="dt"class="card-text"><?php echo $js->getTime() ?></p>
-                            
                         </div>
-                    </div>
                 </div>
-                <div class="col-sm">
+            </div>
+            <div class="col-sm">
                     <div class="col-sm">
                         <div class="card text-center" style="width: 18rem;">
                                 <div class="card-body">
                                     <h5 class="card-title">REFUEL COUNT</h5>
                                     <p id="rc"class="card-text"><?php echo $js->getCount()?></p>
-                                    
                                 </div>
                         </div>
                     </div>
-                </div>
+            </div>
         </div>
-        <div class="row">
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">UID</th>
-      <th scope="col">Distance</th>
-      <th scope="col">Time</th>
-      <th scope="col">Refuel Count</th>
-      <th scope="col">Road Length</th>
-      <th scope="col">Road Type</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php 
-      $query="select * from logs order by uid desc";
-      $result=mysqli_query($connection,$query);
-      $row_no =1;
-      echo "<div class='container vertical-scrollable'>";
-      while($row=mysqli_fetch_assoc($result)){
-    echo "<tr>";
-    echo "<th scope='row' >{$row_no}</td>";
-    echo "<td>{$row['uid']}</td>";
-    echo "<td>{$row['dist']}</td>";
-    echo "<td>{$row['time']}</td>";
-    echo "<td>{$row['count']}</td>";
-    echo "<td>{$row['roadLength']}</td>";
-    echo "<td>{$row['roadType']}</td>";
-    echo "</tr>";
-    $row_no = $row_no+1;
-      }
-      echo "</div>";
-    ?>
-    
+        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search by uid.." title="Type in a unique id">
+        <div  class='table-wrapper-scroll-y my-custom-scrollbar'>
+            <table id="myTable" class='table table-bordered table-striped mb-0'>
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">UID</th>
+                    <th scope="col">Distance</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Refuel Count</th>
+                    <th scope="col">Road Length</th>
+                    <th scope="col">Road Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    $query="select * from logs order by uid desc";
+                    $result=mysqli_query($connection,$query);
+                    $row_no =1;
+                    
+                    
+                    while($row=mysqli_fetch_assoc($result)){
+                    echo "<tr>";
+                    echo "<th scope='row' >{$row_no}</td>";
+                    echo "<td>{$row['uid']}</td>";
+                    echo "<td>{$row['dist']}</td>";
+                    echo "<td>{$row['time']}</td>";
+                    echo "<td>{$row['count']}</td>";
+                    echo "<td>{$row['roadLength']}</td>";
+                    echo "<td>{$row['roadType']}</td>";
+                    echo "</tr>";
+                    $row_no = $row_no+1;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-      </tbody>
-</table>
-
-    
-
-
-
    
-    
+
 
 </body>
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+</script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
